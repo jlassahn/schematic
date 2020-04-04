47e18e6dc9107e214fdf4cf9d914ee6c322017aa
Miacid <jkl@Jeffs-Mac.local>
2020-04-03 21:03:38 -0700

    reorganize schematic view to separate out schematic box.

--- viewer/schemview.go
+++ viewer/schemview.go
@@ -8,6 +8,8 @@ import (
 	"github.com/jlassahn/schematic"
 )
 
+// FIXME destroy
+
 func CreateSchematicViewFromFile(filename string) (*SchematicView, error) {
 
 	schem, err := schematic.LoadSchematic(filename)
@@ -16,9 +18,26 @@ func CreateSchematicViewFromFile(filename string) (*SchematicView, error) {
 	}
 
 	ret := SchematicView{}
-	ret.schem = schem
-	ret.zoom = 1.0
-	ret.currentPage = 1
+
+	window := gogui.CreateWindow(gogui.WINDOW_NORMAL)
+	ret.window = window
+
+	window.SetPosition(
+		gogui.Pos(50, 0),
+		gogui.Pos(10, 0),
+		gogui.Pos(75, 0),
+		gogui.Pos(50, 0))
+	window.HandleClose(ret.closeHandler)
+
+	ret.schemBox = CreateSchematicBox(schem)
+	ret.schemBox.Scroll.SetPosition( // FIXME encapsulate
+		gogui.Pos(0,100),
+		gogui.Pos(0,50),
+		gogui.Pos(100,0),
+		gogui.Pos(100,0))
+
+	window.AddChild(ret.schemBox.Scroll)
+
 
 	menu := gogui.CreateMenu()
 	submenu := menu.GetApplicationMenu()
@@ -39,40 +58,17 @@ func CreateSchematicViewFromFile(filename string) (*SchematicView, error) {
 	submenu = gogui.CreateTextMenuItem("View")
 		item = gogui.CreateTextMenuItem("Zoom In")
 		item.SetShortcut("+")
-		item.HandleMenuSelect(ret.zoomIn)
+		item.HandleMenuSelect(ret.schemBox.ZoomIn)
 		submenu.AddMenuItem(item)
 
 		item = gogui.CreateTextMenuItem("Zoom Out")
-		item.HandleMenuSelect(ret.zoomOut)
+		item.HandleMenuSelect(ret.schemBox.ZoomOut)
 		item.SetShortcut("-")
 		submenu.AddMenuItem(item)
 		menu.AddMenuItem(submenu)
 
-	window := gogui.CreateWindow(gogui.WINDOW_NORMAL)
 	window.SetMenu(menu)
 
-	ret.window = window
-	window.SetPosition(
-		gogui.Pos(50, 0),
-		gogui.Pos(10, 0),
-		gogui.Pos(75, 0),
-		gogui.Pos(50, 0))
-	window.HandleClose(ret.closeHandler)
-
-	scroll := gogui.CreateScrollBox()
-	ret.scroll = scroll
-	scroll.SetPosition(
-		gogui.Pos(0,100),
-		gogui.Pos(0,50),
-		gogui.Pos(100,0),
-		gogui.Pos(100,0))
-
-	dx := int(float64(schem.Settings.PageWidth)*ret.zoom + 40.0)
-	dy := int(float64(schem.Settings.PageHeight)*ret.zoom + 40.0)
-	scroll.SetContentSize(dx, dy)
-	scroll.SetBackgroundColor(gogui.Color{128, 128, 128, 255})
-	scroll.HandleRedraw(ret.drawHandler)
-	window.AddChild(scroll)
 
 	btnPos := 50
 	button := gogui.CreateTextButton("Button 1")
@@ -112,11 +108,9 @@ func CreateSchematicViewFromFile(filename string) (*SchematicView, error) {
 }
 
 type SchematicView struct {
-	zoom float64
-	schem *schematic.Schematic
 	window gogui.Window
-	scroll gogui.ScrollBox
-	currentPage int
+
+	schemBox *SchematicBox
 }
 
 func (view *SchematicView) Close() error {
@@ -130,46 +124,83 @@ func (view *SchematicView) clickHandler() {
 }
 
 func (view *SchematicView) closeHandler() {
-	view.window.Destroy()
-	ViewListRemove(view)
+	view.Close()
 }
 
-func (view *SchematicView) drawHandler(gfx gogui.Graphics) {
+type MouseMode interface {
+	MouseMove(x int, y int)
+	MouseDown(x int, y int, btn int)
+	MouseUp(x int, y int, btn int)
+	Cancel()
+}
+
+type SchematicBox struct {
+	Scroll gogui.ScrollBox
+	zoom float64
+	schem *schematic.Schematic
+	currentPage int
+
+	mouseMode MouseMode
+}
+
+func CreateSchematicBox(schem *schematic.Schematic) *SchematicBox {
+	ret := SchematicBox{}
+	ret.zoom = 1.0
+	ret.schem = schem
+	ret.currentPage = 1
+
+	ret.Scroll = gogui.CreateScrollBox()
+	dx := int(float64(schem.Settings.PageWidth)*ret.zoom + 40.0)
+	dy := int(float64(schem.Settings.PageHeight)*ret.zoom + 40.0)
+	ret.Scroll.SetContentSize(dx, dy)
+	ret.Scroll.SetBackgroundColor(gogui.Color{128, 128, 128, 255})
+	ret.Scroll.HandleRedraw(ret.drawHandler)
+	ret.Scroll.HandleMouseMove(ret.mouseMoveHandler)
+	ret.Scroll.HandleMouseDown(ret.mouseDownHandler)
+	ret.Scroll.HandleMouseUp(ret.mouseUpHandler)
+	ret.Scroll.HandleMouseEnter(ret.mouseEnterHandler)
+	ret.Scroll.HandleMouseLeave(ret.mouseLeaveHandler)
+
+	return &ret
+
+}
+
+func (box *SchematicBox) ZoomIn() {
+	box.zoomTo(box.zoom*2)
+}
+
+func (box *SchematicBox) ZoomOut() {
+	box.zoomTo(box.zoom/2)
+}
+
+func (box *SchematicBox) drawHandler(gfx gogui.Graphics) {
 
 	fmt.Println("DRAW")
 
-	dc := DrawingContext{gfx, view.zoom, 20.0}
-	width := view.schem.Settings.PageWidth
-	height := view.schem.Settings.PageHeight
+	dc := DrawingContext{gfx, box.zoom, 20.0}
+	width := box.schem.Settings.PageWidth
+	height := box.schem.Settings.PageHeight
 
 	dc.DrawOutline(width, height)
 	schematic.DrawGrid(dc, width, height)
-	view.schem.DrawPage(dc, view.currentPage)
+	box.schem.DrawPage(dc, box.currentPage)
 }
 
-func (view *SchematicView) zoomIn() {
-	view.zoomTo(view.zoom*2)
-}
-
-func (view *SchematicView) zoomOut() {
-	view.zoomTo(view.zoom/2)
-}
-
-func (view *SchematicView) zoomTo(zm float64) {
+func (box *SchematicBox) zoomTo(zm float64) {
 
 	fmt.Println("Zoom")
 
-	old_zoom := view.zoom
-	old_dx := int(float64(view.schem.Settings.PageWidth)*view.zoom + 40.0)
-	old_dy := int(float64(view.schem.Settings.PageHeight)*view.zoom + 40.0)
-	view.zoom = zm
-	dx := int(float64(view.schem.Settings.PageWidth)*view.zoom + 40.0)
-	dy := int(float64(view.schem.Settings.PageHeight)*view.zoom + 40.0)
+	old_zoom := box.zoom
+	old_dx := int(float64(box.schem.Settings.PageWidth)*box.zoom + 40.0)
+	old_dy := int(float64(box.schem.Settings.PageHeight)*box.zoom + 40.0)
+	box.zoom = zm
+	dx := int(float64(box.schem.Settings.PageWidth)*box.zoom + 40.0)
+	dy := int(float64(box.schem.Settings.PageHeight)*box.zoom + 40.0)
 
-	visible_width := view.scroll.GetVisibleWidth()
-	visible_height := view.scroll.GetVisibleHeight()
-	visible_left := view.scroll.GetVisibleLeft()
-	visible_top := view.scroll.GetVisibleTop()
+	visible_width := box.Scroll.GetVisibleWidth()
+	visible_height := box.Scroll.GetVisibleHeight()
+	visible_left := box.Scroll.GetVisibleLeft()
+	visible_top := box.Scroll.GetVisibleTop()
 	if visible_width > old_dx {
 		visible_width = old_dx
 	}
@@ -192,9 +223,29 @@ func (view *SchematicView) zoomTo(zm float64) {
 		top = 0
 	}
 
-	view.scroll.SetContentSize(dx, dy)
-	view.scroll.SetVisibleLeftTop(left, top)
+	box.Scroll.SetContentSize(dx, dy)
+	box.Scroll.SetVisibleLeftTop(left, top)
 
-	view.scroll.ForceRedraw()
+	box.Scroll.ForceRedraw()
+}
+
+func (box *SchematicBox) mouseMoveHandler(x int, y int) {
+	fmt.Printf("MOUSE MOVE %v, %v\n", x, y)
+}
+
+func (box *SchematicBox) mouseDownHandler(x int, y int, btn int) {
+	fmt.Printf("MOUSE DOWN %v, %v, %v\n", x, y, btn)
+}
+
+func (box *SchematicBox) mouseUpHandler(x int, y int, btn int) {
+	fmt.Printf("MOUSE UP %v, %v, %v\n", x, y, btn)
+}
+
+func (box *SchematicBox) mouseEnterHandler() {
+	fmt.Printf("MOUSE ENTER\n")
+}
+
+func (box *SchematicBox) mouseLeaveHandler() {
+	fmt.Printf("MOUSE LEAVE\n")
 }
 
